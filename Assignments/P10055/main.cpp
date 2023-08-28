cc27e2234a0b1998c08fe75147057a07ac9c075e
Michelle <100542045+michelle083@users.noreply.github.com>
2023-08-28 18:23:46 -0500

    Create main.cpp

---
+++ Assignments/P10055/main.cpp
@@ -0,0 +1,17 @@
+/**
+* Michelle Orru
+* CMPS 4883 - Programming Techniques
+* 08/28/2023
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long x, y;
+
+  while (cin >> x >> y) {
+    cout << abs(x - y) << endl;
+  }
+}
